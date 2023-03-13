55a1f09dea2d27552019762d879414b18ce18d5d
MukilMuki <mukilan.shanmugasundaram@cdw.com>
2023-03-13 08:59:33 +0530

    cursor pointer for icon

--- blender-movie_JSON/assets/js/script.js
+++ blender-movie_JSON/assets/js/script.js
@@ -1,19 +1,3 @@
-
-posterFile =[
-    {
-        "imageUrl" : "https://ddz4ak4pa3d19.cloudfront.net/cache/56/62/5662bc2079ee7ffd491b65c29a360ac9.jpg",
-        "title" : "Sprite Fright"
-    },
-    {
-        "imageUrl" : "https://cdna.artstation.com/p/assets/covers/images/019/392/064/large/andy-goralczyk-agent-poster-street-small.jpg?1563288236",
-        "title" : "Agent 327"
-    },
-    {
-        "imageUrl" : "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Spring2019AlphaPosterBlender.jpg/1200px-Spring2019AlphaPosterBlender.jpg",
-        "title" : "Spring"
-    }
-]
-
 $(document).ready(function () {
 const reviewerComments = $(".comments-section");
 $.getJSON('/blender-movie_JSON/assets/js/video.json',function(json){
@@ -61,8 +45,10 @@ $.getJSON('/blender-movie_JSON/assets/js/video.json',function(json){
 
 
 const posterProject = $("div.upcoming-projects__posters");
-$(posterFile).each(function(){
+$.getJSON('/blender-movie_JSON/assets/js/posters.json',function(poster){
+    $.each(poster, function(){
     let poster = $(`<img src=${this.imageUrl} class=poster-image>`);
     posterProject.append(poster);
+});
 });
 });
